cfcfb4994bf316aa64e1accf7b987a13618dc610
Paulo <pdichone@gmail.com>
2024-02-06 11:39:32 -0800

    Add AlertDialog and TextField

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,18 +8,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final amountController = TextEditingController();
+  void addWater() {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              title: const Text('Add Water'),
+              content: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('Add water to your daily intake'),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Amount'),
+                  )
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Cancel')),
+                TextButton(onPressed: () {
+                  //save data to db
+                }, child: Text('Save')),
+              ],
+            ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 4,
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.map))],
-        title: Text('Water'),
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.map))],
+        title: const Text('Water'),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      floatingActionButton:
-          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+          onPressed: addWater, child: const Icon(Icons.add)),
     );
   }
 }
