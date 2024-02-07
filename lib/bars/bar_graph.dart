4e01337d29aa479bc194df62894ad03bdc0ebed0
Paulo <pdichone@gmail.com>
2024-02-07 10:56:16 -0800

    Add days of the week on the bottom of the graph

--- lib/bars/bar_graph.dart
+++ lib/bars/bar_graph.dart
@@ -44,12 +44,18 @@ class BarGraph extends StatelessWidget {
           minY: 0,
           gridData: const FlGridData(show: false),
           borderData: FlBorderData(show: false),
-          titlesData: const FlTitlesData(
+          titlesData: FlTitlesData(
               show: true,
-              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              topTitles:
+                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              leftTitles:
+                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
               rightTitles:
-                  AxisTitles(sideTitles: SideTitles(showTitles: false))),
+                  const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+              bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                      showTitles: true,
+                      getTitlesWidget: getBottomTitlesWidget))),
           barGroups: barData.barData
               .map((data) => BarChartGroupData(x: data.x, barRods: [
                     BarChartRodData(
@@ -64,5 +70,69 @@ class BarGraph extends StatelessWidget {
                   ]))
               .toList())),
     );
+  }
+
+  Widget getBottomTitlesWidget(double value, TitleMeta meta) {
+    const TextStyle style = TextStyle(
+        color: Color.fromARGB(255, 24, 23, 23),
+        fontWeight: FontWeight.bold,
+        fontSize: 12);
+
+    Widget text;
+
+    switch (value.toInt()) {
+      case 0:
+        text = const Text(
+          'S',
+          style: style,
+        );
+        break;
+      case 1:
+        text = const Text(
+          'M',
+          style: style,
+        );
+        break;
+
+      case 2:
+        text = const Text(
+          'T',
+          style: style,
+        );
+        break;
+
+      case 3:
+        text = const Text(
+          'W',
+          style: style,
+        );
+        break;
+
+      case 4:
+        text = const Text(
+          'T',
+          style: style,
+        );
+        break;
+
+      case 5:
+        text = const Text(
+          'F',
+          style: style,
+        );
+        break;
+
+      case 6:
+        text = const Text(
+          'S',
+          style: style,
+        );
+        break;
+      default:
+        text = const Text('');
+        break;
+    }
+
+    return SideTitleWidget(space: 3, axisSide: meta.axisSide, child: text);
   }
 }
