c87c7bffb1dc716f509b36fdf292b6c478952689
Chimuka Moonde <chimukamoonde@gmail.com>
2024-04-10 22:46:54 +0200

    minor adjustments

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -5,6 +5,7 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Settings')));
+    return Scaffold(
+        appBar: AppBar(title: const Text('Settings')));
   }
 }
